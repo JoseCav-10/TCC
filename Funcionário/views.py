81a769cd9e81ec7000fc0f725c10b3f9b87f2014
TioCaminhao <savio.v@escolar.ifrn;edu.br>
2024-09-29 00:46:08 -0300

    adicionando templates

--- Funcionário/views.py
+++ Funcionário/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def pedidos(request):
+      return render(request, 'escola/pedidosadmin.html')
+
+def menu(request):
+      return render(request, 'escola/menuadmin.html')
+
+def formuadmin(request): 
+      return render(request, 'escola/formularioadmin.html')
+
+def calendario(request):
+      return render(request, 'escola/calendario.html')
